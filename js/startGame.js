4c3fdcb8306e98ee871857d661d2e876233c4bc2
Andrey Zuev <stepgt@mail.ru>
2024-07-05 15:34:20 +1000

    Added addEventListener to all cards & Added flip logic

--- js/startGame.js
+++ js/startGame.js
@@ -1,5 +1,6 @@
 import { createIconsArray, duplicateArray, shuffle } from './utils.js';
 import { createGameCard } from './gameCard.js';
+import { createGameMenu } from './gameMenu.js';
 
 export const startGame = (difficult) => {
   let firstCard  = null;
@@ -23,4 +24,50 @@ export const startGame = (difficult) => {
   duplicatedCardsIcons.forEach(icon => gameTable.append(createGameCard('question-circle', icon)));
 
   gameSection.append(gameTable, restartBtn);
+
+  const cards = document.querySelectorAll('.game-card');
+
+  restartBtn.addEventListener('click', createGameMenu);
+
+  cards.forEach((card, index) =>
+    card.addEventListener('click', () => {
+      if (clickable == true && !card.classList.contains('successfully')) {
+        card.classList.add('flip');
+
+        if (firstCard == null) {
+          firstCard = index;
+        } else {
+          if (index != firstCard) {
+            secondCard = index;
+            clickable = false;
+          }
+        }
+
+        if (firstCard != null && secondCard != null && firstCard != secondCard) {
+          if (
+            cards[firstCard].firstElementChild.className ===
+            cards[secondCard].firstElementChild.className
+          ) {
+            setTimeout(() => {
+              cards[firstCard].classList.add('successfully');
+              cards[secondCard].classList.add('successfully');
+
+              firstCard = null;
+              secondCard = null;
+              clickable = true;
+            }, 500);
+          } else {
+            setTimeout(() => {
+              cards[firstCard].classList.remove('flip');
+              cards[secondCard].classList.remove('flip');
+
+              firstCard = null;
+              secondCard = null;
+              clickable = true;
+            }, 500);
+          }
+        }
+      }
+    }),
+  );
 };
